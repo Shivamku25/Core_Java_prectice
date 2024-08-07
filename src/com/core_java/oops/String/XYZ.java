0df404e732794e3b6f0e2ec60f62132b1e60e42a
shivamku25 <shivam.25.kumr@gmail.com>
2024-08-07 09:29:23 +0530

    AppletLifeCycle program in core java.

---
+++ src/com/core_java/oops/String/XYZ.java
@@ -0,0 +1,11 @@
+package com.core_java.oops.String;
+
+public class XYZ {
+	public static void main(String[] args) {
+		String A = "Hello";
+		String B = " India";
+		String C = A+B;
+		System.out.println("Sum of two String -> " +C);
+	}
+
+}
