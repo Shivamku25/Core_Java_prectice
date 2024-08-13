a76f5c8351565c59bccb9dcf29e261da419c5d0e
shivamku25 <shivam.25.kumr@gmail.com>
2024-08-13 19:41:08 +0530

    AppletLifeCycle program in core java.

---
+++ src/com/core_java/oops/Java/Test1.java
@@ -0,0 +1,8 @@
+package com.core_java.oops.Java;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int String = 888;
+        System.out.println(String);
+    }
+}
---
+++ src/com/core_java/oops/String/Test1.java
@@ -0,0 +1,11 @@
+package com.core_java.oops.String;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String s1 = 10+20+"ab"+"cd"+11+12;
+		{
+			System.out.println(s1);
+		}
+	}
+}
