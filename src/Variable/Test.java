6d719d92777b650ad685c97b4dba4d853ae9295d
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-01 19:42:47 +0530

    call instance variable to create class object

---
+++ src/Variable/Test.java
@@ -0,0 +1,12 @@
+package Variable;
+
+public class Test
+{
+    int x = 10;
+    public static void main(String[] args) {
+        //System.out.println(x);
+        Test t=new Test();
+        System.out.println(t.x);
+
+    }
+}
