b6c02736e98ebd1bb96f66a177de42fbe025f8ce
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-07 17:55:00 +0530

    check given number is true or false in floating floating point operator

---
+++ src/Operator_Assignments/Increment_Decrement.java
@@ -0,0 +1,18 @@
+package Operator_Assignments;
+
+public class Increment_Decrement {
+    public static void main(String[] args) {
+        int x=10;
+        char ch = 'a';
+        double d = 10.5;
+        int y=++x;
+        ch++;
+        --d;
+         x++;
+         y--;
+        System.out.println(y);
+        System.out.println(x);
+        System.out.println(ch);
+        System.out.println(d);
+    }
+}
