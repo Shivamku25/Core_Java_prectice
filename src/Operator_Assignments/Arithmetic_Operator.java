ebc10adec30e48a481f16dff66971d14565d3e04
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-07 17:39:53 +0530

    Use of arithmetic operator

---
+++ src/Operator_Assignments/Arithmetic_Operator.java
@@ -0,0 +1,13 @@
+package Operator_Assignments;
+
+public class Arithmetic_Operator {
+   //char a,b;
+    //double d;
+    public static void main(String[] args) {
+        System.out.println('a'+'b');//char value automatically converted into integer value using ascii.
+        System.out.println('a'+10.5);
+//        System.out.println(10/0); RE: ArithmeticException:/by zero.
+        System.out.println(0/0);
+
+    }
+}
