cddcf1543344cb3cecadbdbe99194cba72a5c2e6
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:44:03 +0530

    Boolean_Data_Type_Using_If_Else

---
+++ src/Flow_Control/Boolean_Data_Type_Using_If_Else.java
@@ -0,0 +1,15 @@
+package Flow_Control;
+
+public class Boolean_Data_Type_Using_If_Else {
+    public static void main(String[] args) {
+        boolean b = true;
+        if(b=false)
+        {
+            System.out.println("Hello");
+        }
+        else
+        {
+            System.out.println("Hi");
+        }
+    }
+}
---
+++ src/Flow_Control/Boolean_Data_Type_Using_If_Else2.java
@@ -0,0 +1,15 @@
+package Flow_Control;
+
+public class Boolean_Data_Type_Using_If_Else2 {
+    public static void main(String[] args) {
+        boolean b = false;
+        if(b==false)
+        {
+            System.out.println("Hello");
+        }
+        else
+        {
+            System.out.println("Hi");
+        }
+    }
+}
