05acb5c2bb542b8306fc8a4a8a21bf1ace5d0fe4
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:48:49 +0530

    conditional check statement

---
+++ src/Flow_Control/Conditional_Check_Statement.java
@@ -0,0 +1,10 @@
+package Flow_Control;
+
+public class Conditional_Check_Statement {
+    public static void main(String[] args) {
+        for (int i=0;true;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
