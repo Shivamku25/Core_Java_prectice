d74d6ff8734bc2d66ca1d07562bd62324b6ea8e2
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:55:28 +0530

    Selection_Statement_Under_The_If_Else

---
+++ src/Flow_Control/Selection_Statement_Under_The_If_Else.java
@@ -0,0 +1,16 @@
+package Flow_Control;
+
+public class Selection_Statement_Under_The_If_Else {
+    public static void main(String[] args) {
+        int x= 10;
+        if(x==20)
+        {
+            System.out.println("Hello");
+        }
+        else
+        {
+
+            System.out.println("Hi");
+        }
+    }
+}
