9c953e6ca03000ab3b25a42e162f1d0dc72d8266
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:51:56 +0530

    To print elements of 1-D array.

---
+++ src/Flow_Control/Enhanced_For_Loop.java
@@ -0,0 +1,12 @@
+package Flow_Control;
+
+public class Enhanced_For_Loop {
+    public static void main(String[] args) {
+        //To print elements of 1-D array.
+        int[] x = {10,20,30,40,50};
+        for (int x1:x)
+        {
+            System.out.println(x1);
+        }
+    }
+}
