0bf20e3b1153e2ce36e8e160bae8481de03be24e
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:54:06 +0530

    Multiple_If_Else

---
+++ src/Flow_Control/Multiple_If_Else.java
@@ -0,0 +1,15 @@
+package Flow_Control;
+
+public class Multiple_If_Else
+{
+    public static void main(String[] args) {
+        if (true)
+            if (false)//(true)->Hello,(false)->Hi
+            {
+                System.out.println("Hello");
+            }
+        else {
+                System.out.println("Hi");
+            }
+    }
+}
