ec0d097ea4f843c8c16929a028b19a147d3bf8e1
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:53:47 +0530

    Increment_Decrement_For_Conditional_Check

---
+++ src/Flow_Control/Increment_Decrement_For_Conditional_Check.java
@@ -0,0 +1,11 @@
+package Flow_Control;
+
+public class Increment_Decrement_For_Conditional_Check {
+    public static void main(String[] args) {
+        int i=0;
+        for (System.out.println("Hello");i<3; System.out.println("Hi"))
+        {
+            i++;
+        }
+    }
+}
