183a3ded4b0962953828c27438f0e4609d849dbf
shivamku25 <shivam.25.kumr@gmail.com>
2024-09-10 15:49:40 +0530

    continue statement

---
+++ src/Flow_Control/Continue_Statement.java
@@ -0,0 +1,13 @@
+package Flow_Control;
+
+public class Continue_Statement {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++) {
+            if (i % 2 == 0)
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
